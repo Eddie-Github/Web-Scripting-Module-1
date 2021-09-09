dba99795c2b20cf19a9ce9d3b6011027f152911e
Eddie-Hoffman <50275802+Eddie-Hoffman@users.noreply.github.com>
2021-09-09 14:03:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+//this funtion is called when the button is pressed
+function showInformation() {
+    //make text black so it is visble
+    document.getElementById("List").style.color = "black";
+  }
